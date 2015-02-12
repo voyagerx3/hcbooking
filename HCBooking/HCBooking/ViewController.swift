7586f22f75b367586646b59b92b062af9cfa1d14
Geo Inca <geovanny.inca@hotmail.com>
2015-02-13 00:02:05 +0100

    v001
    Inicio

---
+++ HCBooking/HCBooking/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  HCBooking
+//
+//  Created by Geovanny Inca on 12/02/15.
+//  Copyright (c) 2015 HCBooking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var TxtEmail: UITextField!
+    
+    @IBOutlet weak var TxtPassword: UITextField!
+   
+    @IBOutlet weak var LblError: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func BtnLoginFacebook(sender: AnyObject) {
+    }
+    
+    @IBAction func BtnLogin(sender: AnyObject) {
+        if TxtPassword?.text=="1234"
+        {
+            //let nuevovc=self.storyboard?.instantiateViewControllerWithIdentifier("VCmenu") as UIViewController
+            
+            let viewController:UIViewController = UIStoryboard(name: "VCmenu", bundle: nil).instantiateViewControllerWithIdentifier("ViewController") as UIViewController
+            self.presentViewController(viewController, animated: true, completion: nil)
+        }
+        else
+        {
+            self.LblError.text="Error ....."
+        }
+        
+    }
+    
+    
+    @IBAction func BtnDateAlta(sender: AnyObject) {
+    }
+    
+    
+    
+    
+
+    
+    
+}
+
