ca7b2186b9f1d52c9221d29d39cb9d8052582c00
Geo Inca <geovanny.inca@hotmail.com>
2015-03-05 12:57:23 +0100

    added a new  [B0]

---
+++ HCBooking/HCBooking/Bridge-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridge-Header.h
+//  HCBooking
+//
+//  Created by Geovanny Inca on 03/03/15.
+//  Copyright (c) 2015 HCBooking. All rights reserved.
+//
+
+#ifndef HCBooking_Bridge_Header_h
+#define HCBooking_Bridge_Header_h
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
--- HCBooking/HCBooking/ViewController.swift
+++ HCBooking/HCBooking/ViewController.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var TxtEmail: UITextField!
+class ViewController: UIViewController, FBLoginViewDelegate  {
     
+    @IBOutlet var fbLoginView : FBLoginView!
+    @IBOutlet var profilePictureView : FBProfilePictureView!
+
+
+    
+    @IBOutlet weak var TxtEmail: UITextField!
     @IBOutlet weak var TxtPassword: UITextField!
-   
     @IBOutlet weak var LblError: UILabel!
     
     
@@ -21,7 +24,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.fbLoginView.delegate=self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+        
     }
+    
+    
+    
+    //Facebook Delegate Methods
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+        println("This is where you perform a segue.")
+        
+        //                    let nuevovc=self.storyboard?.instantiateViewControllerWithIdentifier("VCMenu") as VCMenu
+        //                    self.presentViewController(nuevovc, animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser){
+        println("User Name: \(user.name)")
+        profilePictureView.profileID = user.objectID
+        LblError.text=user.name
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        LblError.text="User Logged Out"
+        profilePictureView.profileID = nil
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+    
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
